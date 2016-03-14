b699886aaf6e9142dd47710c8bbaf1c31d720be5
joezuntz <joezuntz@googlemail.com>
2016-03-14 10:45:32 -0700

    avoid crashes for missing modules and include style file

--- src/fig.py
+++ src/fig.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib
 matplotlib.use ('agg')
 import matplotlib.pyplot as plt
-plt.style.use('/home/troxel/SVA1/SVA1StyleSheet.mplstyle')
+import os
+dirname=os.path.split(__file__)[0]
+style_file=os.path.join(dirname, "SVA1StyleSheet.mplstyle")
+plt.style.use(style_file)
 import matplotlib.cm as cm
 from matplotlib.colors import LogNorm
 import matplotlib.gridspec as gridspec
