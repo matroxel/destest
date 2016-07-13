1f50247864edf0754f9f7795e0f8955f9d856b43
Michael Troxel <michael.troxel@manchester.ac.uk>
2016-07-13 16:18:47 +0100

    testing

--- src/corr.py
+++ src/corr.py
@@ -1099,6 +1099,11 @@ class runs(object):
     cate=treecorr.Catalog(g1=shape.e1[maske]-shape.c1[maske], g2=shape.e2[maske]-shape.c2[maske], w=shape.w[maske], ra=shape.ra[maske], dec=shape.dec[maske], r=shape.r[maske], ra_units='deg', dec_units='deg')
     catm=treecorr.Catalog(k=(1.+shape.m[maske]), w=shape.w[maske], ra=shape.ra[maske], dec=shape.dec[maske], r=shape.r[maske], ra_units='deg', dec_units='deg')
 
+    nreg=int(np.max(pos.reg)+1)
+    r=np.zeros((4,nreg,bins))
+    xi=np.zeros((4,nreg,bins))
+    xi_im=np.zeros((4,nreg,bins))
+    weight=np.zeros((4,nreg,bins))
     for i in range(nreg):
       print i
       maskd0=maskd&(pos.reg==i)
