172b7d99b95fc75fef4fec10d3675e2cecb293d9
troxel <michael.a.troxel@gmail.com>
2016-11-29 17:28:58 -0500

    update for matched catalog

--- src/catalog.py
+++ src/catalog.py
@@ -672,7 +672,7 @@ class CatalogMethods(object):
     shapearray=shapearray[goldmask&shapemask]
 
     import numpy.lib.recfunctions as nlr
-    array=nlr.append_fields(goldarray,shapearray)
+    array=nlr.append_fields(goldarray,shapearray.dtype.names,shapearray)
 
     goldarray.dtype.names = (goldtable[key] for key in goldcols)
     shapearray.dtype.names = (shapetable[key] for key in shapecols if key is not 'coadd')
