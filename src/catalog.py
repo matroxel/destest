8e068073bd117b0b35e6b8a7f61bf0c767cc04ba
troxel <michael.a.troxel@gmail.com>
2016-12-05 15:31:58 -0500

    corrections

--- src/catalog.py
+++ src/catalog.py
@@ -88,7 +88,6 @@ class CatalogStore(object):
         goldcols=np.array(list(config.matched_gold_col_lookup.keys()))
 
         cols1=[table.get(x,x) for x in cols]
-        print cols
         cols2,catcols,filenames,filenums=CatalogMethods.get_matched_cat_cols(goldfile,catfile,goldcols,cols,config.matched_gold_col_lookup,table,cutfunc,tiles=tiles,maxrows=maxrows,maxiter=maxiter,exiter=exiter,hdu=hdu)
 
         for i,x in enumerate(catcols):
@@ -628,10 +627,8 @@ class CatalogMethods(object):
     tmparray = goldfits[hdu].read(columns=['FLAGS_GOLD','FLAGS_BADREGION'])
     goldmask = (tmparray['FLAGS_GOLD']==0)&(tmparray['FLAGS_BADREGION']==0)&(np.arange(len(tmparray))<maxrows)
 
-    print shapecols,[shapetable.get(x,x) for x in shapecols]
     # Verify that the columns requested exist in the file
     colex,colist=CatalogMethods.col_exists([shapetable.get(x,x) for x in shapecols],shapefits[hdu].get_colnames())
-    print 'columns '+colist+' do not exist in file: '+shape
     if colex<1:
       for i,x in enumerate(shapecols):
         shapecols[i]=x.lower()
