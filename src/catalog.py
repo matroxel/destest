6519921463940483950f47cef30dda7443970d4d
troxel <michael.a.troxel@gmail.com>
2016-12-05 15:40:09 -0500

    corrections

--- src/catalog.py
+++ src/catalog.py
@@ -871,6 +871,8 @@ class CatalogMethods(object):
   @staticmethod
   def matched_i3_cut():
     cuts=CatalogMethods.add_cut(np.array([]),'flags',noval,0,noval)
+    cuts=CatalogMethods.add_cut(cuts,'snr',12,noval,200)
+    cuts=CatalogMethods.add_cut(cuts,'rgp',1.13,noval,3.0)
 
     return cuts
 
