9155560338f3f32b41e7ab2eaff81eab3923331a
troxel <michael.a.troxel@gmail.com>
2016-12-12 18:22:23 +0000

    debugging

--- src/field.py
+++ src/field.py
@@ -129,10 +129,10 @@ class field(object):
         w=np.ones(np.sum(mask))
       e10,x0,y0=np.histogram2d(cat.row[mask],cat.col[mask],bins=[nx,ny],weights=e1*w)
       e20,x0,y0=np.histogram2d(cat.row[mask],cat.col[mask],bins=[nx,ny],weights=e2*w)
-      print e1,e2,w,np.sqrt(e1**2+e2**2)
       e0,x0,y0=np.histogram2d(cat.row[mask],cat.col[mask],bins=[nx,ny],weights=np.sqrt(e1**2+e2**2)*w)
       mw,x0,y0=np.histogram2d(cat.row[mask],cat.col[mask],bins=[nx,ny],weights=m*w)
 
+      print x[i,:,:], x0[1:], x0[:-1] 
       x[i,:,:]=(x0[1:]+x0[:-1])/2
       y[i,:,:]=(y0[1:]+y0[:-1])/2
 
