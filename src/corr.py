3750ecdb817d9963a49e79148f07d001952bb4f7
troxel <michael.a.troxel@gmail.com>
2017-02-02 23:55:00 -0500

    fix

--- src/corr.py
+++ src/corr.py
@@ -199,7 +199,7 @@ class xi_2pt(object):
       RS1=(RS1p.xi-RS1m.xi)/(2.*config.cfg.get('mcal_dg'))
       RS2=(RS2p.xi-RS2m.xi)/(2.*config.cfg.get('mcal_dg'))
       print '3',Rg.xi,(RS1+RS2)/2.
-      return (Rg.xi+(RS1+RS2)/2.)/2
+      return (Rg.xi+(RS1+RS2)/2.)
 
     # def mcal_norm_4(cata,catxa,catRga,w,maska):
 
@@ -226,8 +226,8 @@ class xi_2pt(object):
       Rg.process(catRga,catRga)
       catRga=None
       print 'after rg run',time.time()-t0
-      print '5',Rg.xip
-      return Rg.xip
+      print '5',Rg.xip/2
+      return Rg.xip/2
 
 
     if cata.cat=='mcal':
