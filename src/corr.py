9ca8f41ba7b0ae529ee737698f2e20bd8a76a830
troxel <michael.a.troxel@gmail.com>
2017-02-21 14:19:10 -0500

    testing

--- src/corr.py
+++ src/corr.py
@@ -351,12 +351,10 @@ class xi_2pt(object):
         gb='e'
       if conj:
         e2=-e2
-      print wb
       e1,e2,w,m1,m2=lin.linear_methods.get_lin_e_w_ms(catb,xi=True,mock=mock,mask=maskb,w1=wb)
-      print w
 
       if corr in ['GG','NG','KG']:
-        print e1/m1, e2/m2, w,catb.ra,catb.dec,maskb0
+        print len(e1/m1),len(w),len(catb.ra),len(catb.dec),np.max(maskb0)
         catxb=treecorr.Catalog(g1=e1/m1, g2=e2/m2, w=w[maskb0], ra=catb.ra[maskb0], dec=catb.dec[maskb0], ra_units='deg', dec_units='deg')
         if (catb.cat=='mcal')&(catb.bs):
           #catRgb=treecorr.Catalog(k=(m1+m2)/2., w=w[maskb0], ra=catb.ra[maskb0], dec=catb.dec[maskb0], ra_units='deg', dec_units='deg')
