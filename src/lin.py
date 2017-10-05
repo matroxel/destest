9f546f3dcf242d399b56449996c1643221620e37
troxel <michael.a.troxel@gmail.com>
2017-10-04 21:23:58 -0400

    push

--- src/lin.py
+++ src/lin.py
@@ -163,6 +163,7 @@ class linear_methods(object):
           m2=np.mean(cat.R22[mask0]*w[0][mask0]) #np.mean(cat.e2_2p[mask0])-np.mean(cat.e2_2m[mask0])
           if not isinstance(mask,tuple):
             print 'WARNING: no tuple mask provided, not calculating selection effects....'
+          else:
             m1+=(np.mean(cat.e1[np.append(mask[1],mask[5])]*w[1][np.append(mask[1],mask[5])])-np.mean(cat.e1[np.append(mask[2],mask[5])]*w[2][np.append(mask[1],mask[5])]))/(2.*config.cfg.get('mcal_dg'))
             m2+=(np.mean(cat.e2[np.append(mask[3],mask[5])]*w[3][np.append(mask[1],mask[5])])-np.mean(cat.e2[np.append(mask[4],mask[5])]*w[4][np.append(mask[1],mask[5])]))/(2.*config.cfg.get('mcal_dg'))
           return e1[mask0],e2[mask0],w[0],m1,m2
@@ -173,10 +174,14 @@ class linear_methods(object):
           print 'resp g',m1,m2
           if not isinstance(mask,tuple):
             print 'WARNING: no tuple mask provided, not calculating selection effects....'
+          else:
             m1+=(np.mean(cat.e1[np.append(mask[1],mask[5])])-np.mean(cat.e1[np.append(mask[2],mask[5])]))/(2.*config.cfg.get('mcal_dg'))
             m2+=(np.mean(cat.e2[np.append(mask[3],mask[5])])-np.mean(cat.e2[np.append(mask[4],mask[5])]))/(2.*config.cfg.get('mcal_dg'))
             print 'resp S',(np.mean(cat.e1[np.append(mask[1],mask[5])])-np.mean(cat.e1[np.append(mask[2],mask[5])]))/(2.*config.cfg.get('mcal_dg')),(np.mean(cat.e2[np.append(mask[3],mask[5])])-np.mean(cat.e2[np.append(mask[4],mask[5])]))/(2.*config.cfg.get('mcal_dg'))
-          w=np.ones(len(mask0))
+          if mask0.dtype.type is np.bool_:
+            w=np.ones(np.sum(mask0))
+          else:
+            w=np.ones(len(mask0))
           return e1[mask0],e2[mask0],w,m1,m2
       else:
         m1=1.
@@ -964,6 +969,8 @@ def hist_tests_base(val,x1,mask,x1name,w1=None,x2=None,mask2=None,x2name=None,w2
 
   if config.log_val.get(val,False):
     x1=np.log10(x1[mask&(x1>0)])
+  else:
+    x1=x1[mask]
 
   if x2 is None:
     if w1 is None:
